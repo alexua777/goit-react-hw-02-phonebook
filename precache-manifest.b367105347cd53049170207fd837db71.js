83622cff93deaa6a3ac1e65fcb794d3676d2d866
Alex Bakhteyev <a.bakhteyev@gmail.com>
2020-02-01 22:52:07 -0500

    Updates

---
+++ precache-manifest.b367105347cd53049170207fd837db71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f1dac1121be747f2e597f62bd87c03",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "0e9140e857cf03847608",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.93b16b39.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.93b16b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d696f71b0353f71fd8b9",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.2660fa60.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  }
+]);
